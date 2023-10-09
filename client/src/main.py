04c1bda2542375dbf9947fc46501ecbab99577b7
Lucas Almeida <lucas.andre22@yahoo.com>
2023-10-09 10:16:45 -0300

    Partial final commit with all requirements

--- client/src/main.py
+++ client/src/main.py
@@ -8,98 +8,32 @@ from dotenv import load_dotenv
 import datetime
 import json
 
-load_dotenv() 
-
+load_dotenv()
 os.environ["PYRO_LOGFILE"] = "pyro.log"
 os.environ["PYRO_LOGLEVEL"] = "INFO"
-
 Pyro5.api.config.SERIALIZER="marshal"
 
-def read_user_from_input():
-    name = input("Enter your name: ")
-    public_key = input("Enter your public key: ")
-    remote_uri = input("Enter the remote URI: ")
-
-    return {'name': name, 'public_key': public_key, 'remote_uri': remote_uri}
-
-def read_product_from_input():
-    code = input("Enter the product code: ")
-    name = input("Enter the product name: ")
-    description = input("Enter the product description: ")
-    quantity = int(input("Enter the product quantity: "))
-    unit_price = float(input("Enter the product unit price: "))
-    minimum_stock = int(input("Enter the product minimum stock: "))
-
-    current_datetime = datetime.datetime.now()
-    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-    
-    product = {
-        'code': code,
-        'name': name,
-        'description': description,
-        'quantity': quantity,
-        'unit_price': unit_price,
-        'minimum_stock': minimum_stock,
-        'date': formatted_datetime
-    }
-
-    return product
-
-def read_subtract_input():
-    code = input("Enter the product code to subtract: ")
-    quantity_to_subtract = int(input("Enter the quantity to subtract: "))
-    current_datetime = datetime.datetime.now()
-    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-
-    subtract_request = {
-        'code': code,
-        'quantity': quantity_to_subtract,
-        'date': formatted_datetime
-    }
-
-    return subtract_request
-
-def subtract_product():
-    subtract_request = read_subtract_input()
-    signed_subtract_request = sign_product(subtract_request, private_key)
-    response = server.subtract_product(signed_subtract_request)
-    print(response)
-
-
-def sign_product(product, private_key):
-    product_json = json.dumps(product, sort_keys=True)
-    hash = SHA256.new(product_json.encode())
-    signature = pkcs1_15.new(private_key).sign(hash)
-    product['signature'] = signature.hex()
-
-    return product
-
-def send_signed_product(server, signed_product):
-    response = server.store_new_product(signed_product)
-    return response
-
-
-
-
-
 class Client(object):
-
-    @Pyro5.api.expose
-    def notifyProductEmptying(self):
-        print("")
-
-    @Pyro5.api.expose
-    def notifyProductNotBeingSold(self):
-        print("")
+    def __init__(self, name, id: int):
+        self.name = name
+        self.id = id
     
-message = b'To be signed'
+    def get_id(self) -> int:
+        return self.id
 
+    @Pyro5.api.expose
+    def notify_product_emptying(self, product_emptying):
+        print("Notification from server for user " + self.name + ":")
+        
+        print("")
 
-hash = SHA256.new(message)
+    @Pyro5.api.expose
+    def notify_product_not_being_sold(self):
+        print("")
+        
+current_user: Client = None
+
 private_key = RSA.import_key(open(os.environ["PRIVATE_KEY"]).read())
-signature_object = pkcs1_15.new(private_key)
-signature = signature_object.sign(hash)
-
 public_key = RSA.import_key(open(os.environ["PUBLIC_KEY"]).read())
 
 daemon = Pyro5.api.Daemon()
@@ -109,21 +43,84 @@ print("Ready. Client uri =", uri)
 server = Pyro5.api.Proxy("PYRO:obj_07444d9733eb49eb9b8976ded660baf1@localhost:61765")
 key_base64 = base64.b64encode(public_key.export_key()).decode("utf-8")
 
-products = []
-#add product
-# product = read_product_from_input()
-# signed_product = sign_product(product, private_key)
-# response = send_signed_product(server, signed_product)
-# print(response)
+def sign_message(message, private_key) -> str:
+    message_json = json.dumps(message, sort_keys=True)
+    hash = SHA256.new(message_json.encode())
+    signature = pkcs1_15.new(private_key).sign(hash)
+    message['signature'] = signature.hex()
+
+    return message
+
+def get_current_datetime():
+    current_datetime = datetime.datetime.now()
+    return current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+def read_user_from_input():
+    name = input("Enter your name: ")
+    public_key = input("Enter your public key: ")
+    remote_uri = input("Enter the remote URI: ")
+
+    return {'name': name, 'public_key': public_key, 'remote_uri': remote_uri}
+
+def read_new_user_and_send_to_server():
+    user_info = read_user_from_input()
+    public_key = user_info['public_key']
+    remote_uri = user_info['remote_uri']
+    server.register_user(public_key, remote_uri)
+
+def read_product_from_input():
+    code = input("Enter the product code: ")
+    name = input("Enter the product name: ")
+    description = input("Enter the product description: ")
+    quantity = int(input("Enter the product quantity: "))
+    unit_price = float(input("Enter the product unit price: "))
+    minimum_stock = int(input("Enter the product minimum stock: "))
+    product = {
+        'code': code,
+        'name': name,
+        'description': description,
+        'quantity': quantity,
+        'unit_price': unit_price,
+        'minimum_stock': minimum_stock,
+        'date': get_current_datetime()
+    }
+
+    return product
+    
+def read_new_product_and_send_to_server():
+    if current_user is None:
+        return "Error: a user must be previously registered to make requests"
+    product = read_product_from_input()
+    signed_product = sign_message(product, private_key)
+    return server.store_new_product(signed_product)
+
+def read_product_to_subtract_from_input():
+    code = input("Enter the product code to subtract: ")
+    quantity_to_subtract = int(input("Enter the quantity to subtract: "))
+
+    subtract_request = {
+        'code': code,
+        'quantity': quantity_to_subtract,
+        'date': get_current_datetime()
+    }
+    return subtract_request
+
+def read_product_to_subtract_and_send_to_server():
+    if current_user is None:
+        return "Error: a user must be previously registered to make requests"
+    subtract_request = read_product_to_subtract_from_input()
+    signed_subtract_request = sign_message(subtract_request, private_key)
+    return server.subtract_product(signed_subtract_request, current_user.get_id())
+
 
 while True:
     print("\nMenu:")
     print("1. Registrar Usuario")
-    #necessario ter um usuario antes de por produto
     print("2. Lançamento de entrada de produto")
     print("3. Lançamento de saida de produto")
-    print("4. Periodo de tempo")
-    print("5. Quit")
+    print("4. List available stock")
+    print("5. Periodo de tempo")
+    print("6. Quit")
 
     choice = input("Enter your choice: ")
 
@@ -131,20 +128,27 @@ while True:
         #user_info = read_user_from_input()
         #public_key = user_info['public_key']
         #remote_uri = user_info['remote_uri']
-        #server.register(public_key, remote_uri)
-        server.register(key_base64, uri)  
+        #server.register_user(public_key, remote_uri)
+        read_new_user_and_send_to_server()
+        response = server.register_user(key_base64, uri)
+        if "id" in response:
+            id = response.get("id")
+            current_user = Client(id)
+        print(response)
     elif choice == '2':
-        product = read_product_from_input()
-        signed_product = sign_product(product, private_key)
-        response = send_signed_product(server, signed_product)
-        print(response)  
+        response = read_new_product_and_send_to_server()
+        print(response)
     elif choice == '3':
-        subtract_product()
+        response = read_product_to_subtract_and_send_to_server()
+        print(response)
     elif choice == '4':
-        seconds_to_subtract = int(input("Enter the number of seconds to subtract: "))
+        response = server.get_products_in_stock()
+        print(response)
+    elif choice == '5':
+        seconds_to_subtract = int(input("Enter the time in seconds: "))
         products = server.get_products_after_seconds(seconds_to_subtract)
         print(products)
-    elif choice == '5':
+    elif choice == '6':
         print("Sair")
         break
     else:
--- server/src/main.py
+++ server/src/main.py
@@ -13,6 +13,16 @@ os.environ["PYRO_LOGLEVEL"] = "DEBUG"
 
 Pyro5.api.config.SERIALIZER="marshal"
 
+new_user_id_controller = 0
+
+PERIOD_IN_SECONDS_TO_NOTIFY_CLIENT_PRODUCT_NOT_BEING_SOLD = 20
+
+class ServerClient(object):
+    def __init__(self, name, id):
+        self.public_key = name
+        self.uri = id
+    
+
 class Server:
     client_uri: str
     public_key: str
@@ -21,18 +31,22 @@ class Server:
     operations = []
 
     @Pyro5.api.expose
-    def register(self, public_key, uri):
+    def register_user(self, public_key, uri):
         self.public_key = self.restore_key(public_key=public_key).export_key() #nao sei oq faz
+        print(self.public_key)
         try:
             if public_key in self.users:
-                return 'Error: User already registered.'
+                raise Exception("User already registered")
             self.users[public_key] = {'public_key': public_key, 'uri': uri}
             print(self.users)
-            return 'Success: User registered.'
+            return {"status": "success", "message": "Success: User registered", "id": self.get_new_user_id()}
         except Exception as e:
-            return f'Error registering user: {str(e)}'
+            return {"status": "error", "message" : str(e)}
 
-
+    def get_new_user_id(self):
+        new_user_id_controller += 1
+        return new_user_id_controller-1
+        
     def restore_key(self, public_key):
         return import_key(base64.b64decode(public_key))
 
@@ -66,6 +80,8 @@ class Server:
                     print(self.products)
                     break
             else:
+                # Insert new field that indicates when the product was sold
+                json_product["last_time_sold"] = None
                 self.products.append(json_product)
                 print(f"Added a new product with code {product_code}.")
             
@@ -74,7 +90,15 @@ class Server:
             return 'Error: Invalid signature'
         
     @Pyro5.api.expose
-    def subtract_product(self, json_product):
+    def subtract_product(self, json_product, client_id: str):
+        """_summary_
+
+        Args:
+            json_product (json): { }
+
+        Returns:
+            str: status of operation
+        """
         if self.verifySignature(json_product):
             product_code = json_product.get('code')
             quantity_to_subtract = json_product.get('quantity')
@@ -85,10 +109,19 @@ class Server:
                     if current_quantity >= quantity_to_subtract:
                         product['quantity'] -= quantity_to_subtract
                         print(f"Subtracted {quantity_to_subtract} units from product with code {product_code}.")
+
+                        # Update the last time sold.
+                        product["last_time_sold"] = datetime.now()
+
+                        # Notify client if product reached the minimum stock
+                        if product['quantity'] <= product['minimum_stock']:
+                            print("Product reached minimum stock!")
+
+                        #verifica se 
                         print(self.products)
                         return 'Success: Product subtracted.'
                     else:
-                        return 'Error: Not enough stock to subtract.'
+                        return 'Error: Not enough product in stock to subtract.'
             return 'Error: Product not found.'
 
         else:
@@ -96,30 +129,62 @@ class Server:
         
     @Pyro5.api.expose
     def get_products_after_seconds(self, seconds_to_subtract):
+        """
+        Return list of products given a 
+
+        Returns:
+            _type_: _description_
+        """
         filtered_products = []
         try:
             seconds_to_subtract = int(seconds_to_subtract)
             current_datetime = datetime.now()
             time_difference = timedelta(seconds=seconds_to_subtract)
             result_datetime = current_datetime - time_difference
-            
-            for product in self.products:
-                product_time_str = product.get('date', '')
-                try:
-                    product_time = datetime.strptime(product_time_str, '%Y-%m-%d %H:%M:%S')
-                except ValueError:
-                    continue
-
-                if product_time > result_datetime:
-                    filtered_products.append(product)
-            
-            if not filtered_products:
-                return 'No products found within the specified time period.'
-            
-            return filtered_products
-        
-        except ValueError:
+        except ValueError as e:
+            print(e)
             return 'Error: Invalid input for seconds.'
+
+        for product in self.products:
+            product_time_str = product.get('date', '')
+            try:
+                product_time = datetime.strptime(product_time_str, '%Y-%m-%d %H:%M:%S')
+            except ValueError as e:
+                print(e)
+                continue
+
+            # If 
+            if product_time > result_datetime:
+                filtered_products.append(product)
+
+        if not filtered_products:
+            return 'No products found within the specified time period.'
+
+        return filtered_products
+    
+    @Pyro5.api.expose
+    def get_products_in_stock(self):
+        products_in_stock = []
+        for product in self.products:
+            if product['quantity'] > 0:
+                products_in_stock.append({
+                    'product': product["name"],
+                    'quantity': product["quantity"]
+                })
+
+        return products_in_stock
+    
+    @Pyro5.api.expose
+    def get_products_in_stock(self):
+        products_in_stock = []
+        for product in self.products:
+            if product['quantity'] > 0:
+                products_in_stock.append({
+                    'product': product["name"],
+                    'quantity': product["quantity"]
+                })
+
+        return products_in_stock
 
 
 daemon = Pyro5.api.Daemon()   
@@ -127,3 +192,8 @@ uri = daemon.register(Server)
 
 print("Ready. Object uri =", uri)       
 daemon.requestLoop()
+
+import time
+while True:
+    time.sleep(PERIOD_IN_SECONDS_TO_NOTIFY_CLIENT_PRODUCT_NOT_BEING_SOLD)
+    for()
